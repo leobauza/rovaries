6d15bbeb31eb59ea45bece3ff56c9fcd2c4577ae
Leo Bauza <leob@njimedia.com>
2014-07-20 13:12:28 -0400

    task(styles): resume desktop styles

--- sites/all/themes/stevenrovery/assets/src/controllers/resumeController.js
+++ sites/all/themes/stevenrovery/assets/src/controllers/resumeController.js
@@ -14,17 +14,21 @@
 
     Page.get({'nid':nid}, function (page) {
 
-      var composed = page.node.composed_fields;
+      var composed = page.node.composed_fields,
+          custom = page.node.custom_fields;
 
       $scope.setSiteTitle(page.node.title);
       $scope.setPageTitle(page.node.title);
 
       //update node id for navigation
       $scope.setNid(nid);
-      console.log(page.node);
       $scope.experience = composed.field_experience;
       $scope.schools = composed.field_education;
       $scope.awards = composed.field_awards;
+
+      $scope.resume = custom.field_resume;
+      $scope.arbitrary_links = composed.field_arbitrary_links;
+
 
     });
 
