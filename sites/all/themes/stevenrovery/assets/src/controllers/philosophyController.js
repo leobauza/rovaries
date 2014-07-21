89dd00f2da4a875500d9399a068cb40af31111ea
Leo Bauza <leob@njimedia.com>
2014-07-20 21:29:53 -0400

    task(styles): philsophy slider styles

--- sites/all/themes/stevenrovery/assets/src/controllers/philosophyController.js
+++ sites/all/themes/stevenrovery/assets/src/controllers/philosophyController.js
@@ -14,7 +14,8 @@
         name = $routeParams.name || null,
         nid = $scope.getNid('/' + splitLoc[1]);
 
-    //console.log($scope.nidsMap);
+
+    $scope.slider_philosophy = name; //true or false decides whether to show the slider or front page
 
     Page.get({'nid':nid}, function (page) {
 
@@ -23,9 +24,34 @@
       $scope.setSiteTitle(page.node.title);
       $scope.setPageTitle(page.node.title);
 
-
       //$scope.node = page.node;
-      $scope.slider = page.node.composed_fields.field_philosophy_slider;
+      var slider = page.node.composed_fields.field_philosophy_slider,
+          si = 1; //slide iterator
+
+      $scope.slider_size = _.size(slider);
+
+      $scope.slider = _.map(slider, function (slide) {
+        slide['id'] = si;
+        si += 1;
+        return slide;
+      });
+
+
+
+      var phil_boxes = page.node.composed_fields.field_philosophy_slider,
+          groups = {},
+          i = 0, //iterator
+          gi = 0; //group iterator
+
+      _.each(phil_boxes, function (box) {
+        (i % 3 === 0)? gi += 1 : gi = gi; //increase group iterator by one every 3
+        groups[gi] = groups[gi] || {}; //make sure it exists
+        groups[gi][i] = box; //add view to right group
+        i += 1; //increase iterator by one
+      });
+
+      $scope.phil_groups = groups;
+
     });
 
     $scope.compareToName = function (title) {
@@ -37,6 +63,26 @@
       }
     };
 
+    $scope.getPrevSlide = function (id) {
+
+      if (id - 1 === 0) {
+        return _.where($scope.slider, {id: $scope.slider_size});
+      } else {
+        return _.where($scope.slider, {id: id-1});
+      }
+
+    };
+
+    $scope.getNextSlide = function (id) {
+
+
+      if (id + 1 > $scope.slider_size) {
+        return _.where($scope.slider, {id: 1});
+      } else {
+        return _.where($scope.slider, {id: id+1});
+      }
+
+    };
 
   } ]);
 
